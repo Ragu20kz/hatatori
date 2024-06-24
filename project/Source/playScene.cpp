7dfe4672144985eee323a7b4124c7ba903227fee
K225028_higuchi <k225028@kccollege.ac.jp>
2024-06-24 11:20:27 +0900

    陣地仮配置

--- project/Source/playScene.cpp
+++ project/Source/playScene.cpp
@@ -7,8 +7,11 @@
 #include "ItemManager.h"
 #include "config.h"
 
+#include"territoryManager.h"
+
 PlayScene::PlayScene()
 {
+	TerritoryManager* territory = Instantiate<TerritoryManager>();
 	Player* inst[4];
 	for (int i = 0; i < 4; i++) {
 		inst[i] = Instantiate<Player>();
@@ -39,7 +42,7 @@ void PlayScene::Draw()
 	DrawBox(0, 0, SCREEN_WIDTH, WALL_SIZE, COLOR_WHITE, true);
 	DrawBox(SCREEN_WIDTH, SCREEN_HEIGHT, 0, SCREEN_HEIGHT-WALL_SIZE, COLOR_WHITE, true);
 	DrawBox(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_WIDTH-WALL_SIZE, 0, COLOR_WHITE, true);
-
+	
 //	DrawString(0, 0, "PLAY SCENE", GetColor(255, 255, 255));
 //	DrawString(100, 400, "Push [T]Key To Title", GetColor(255, 255, 255));
 }
---
+++ project/Source/territoryManager.cpp
@@ -0,0 +1,31 @@
+#include "territoryManager.h"
+#include"territory.h"
+
+TerritoryManager::TerritoryManager()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		Territory*t =  new Territory();
+		territory.emplace_back(t);
+	}
+}
+
+TerritoryManager::~TerritoryManager()
+{
+}
+
+void TerritoryManager::Update()
+{
+}
+
+void TerritoryManager::Draw()
+{
+	for (int i = 0; i < territory.size(); i++){
+		territory[i]->Draw();
+	}
+}
+
+void TerritoryManager::SetTerritory(VECTOR _pos, int _num)
+{
+	territory[_num]->positon = _pos;
+}
