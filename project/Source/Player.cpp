087385305fbdedcd1b5a9f56082417fa1a807e4d
伊藤大翔 <k225003@kccollege.ac.jp>
2024-06-24 11:28:37 +0900

    プレイヤーに今後必要になりそうな変数追加

--- project/Source/Player.cpp
+++ project/Source/Player.cpp
@@ -8,6 +8,11 @@ Player::Player()
 	input    = VECTOR();
 	score = 0;
 	type = -1;
+
+	speedBuff = 1.0f;
+	weightMax = 10;//‰¼
+	stanTime = 0.0f;
+	stanInvalid = 0;
 }
 
 Player::~Player()
@@ -16,7 +21,7 @@ Player::~Player()
 
 void Player::Update()
 {
-	position += input * 3.0f;
+	position += input * 3.0f*speedBuff;
 }
 
 void Player::Draw()
--- project/Source/Player.h
+++ project/Source/Player.h
@@ -14,8 +14,13 @@ public:
 	void Input(VECTOR dir);
 private:
 	VECTOR position;
-	int hImage;
+	int hImage;			
 	VECTOR input;
 	int score;
 	int type;
+
+	float speedBuff;	//スピード倍率、デフォルトは1.0f
+	int weightMax;		//重さ上限、これ以上増えると遅くなるよ
+	float stanTime;		//スタン時間
+	int stanInvalid;	//スタン無効回数、複数個保持する可能性があるためとりあえずint
 };
