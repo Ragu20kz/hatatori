7e672f84334763061df990d6e7aedf2699ec07a1
K225028_higuchi <k225028@kccollege.ac.jp>
2024-07-01 10:18:48 +0900

    territoryに色の変数追加

--- project/Source/territory.cpp
+++ project/Source/territory.cpp
@@ -1,7 +1,7 @@
 #include "territory.h"
 
 namespace {
-	static const int BOX_SIZE_X = 200;
+	static const int BOX_SIZE_X = 150;
 	static const int BOX_SIZE_Y = 200;
 }
 
@@ -10,6 +10,7 @@ Territory::Territory()
 	positon.x = 0;
 	positon.y = 0;
 	score = 0;
+	number = 0;
 	color = 0xff0000;
 }
 
@@ -24,5 +25,5 @@ void Territory::Update()
 void Territory::Draw()
 {
 	DrawBox(positon.x, positon.y, positon.x + BOX_SIZE_X, positon.y + BOX_SIZE_Y, color, true);
-	DrawFormatString(positon.x, positon.y, 0xff00ff, "score:%d", score);
+	DrawFormatString(positon.x, positon.y, 0x00ffff, "[%d]:score:%d", number, score);
 }
