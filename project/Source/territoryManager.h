6d0b6ed05ed252409be713e6afa4b520e24892a3
okimasafumi\K225006 <k225006@kccollege.ac.jp>
2024-07-08 10:00:00 +0900

    衝突抜け

--- project/Source/territoryManager.h
+++ project/Source/territoryManager.h
@@ -11,8 +11,6 @@ public:
 	void Draw()override;
 
 	Territory* SetTerritory(const VECTOR& _pos, const int& _num);
-	void ItemCollider();
-	void SetTerritory(VECTOR _pos,int _num);
 	void ItemCollider(); //アイテム乗っている数判定
 	void DebugGUI(); //デバッグ用のGUIを書いとく
 private:
